3f12925ef3cafdb8a089fc1953eb622d9a0d62aa
Nadezhda Solovyeva <nadia.solovyeva@jetbrains.com>
2022-03-14 15:42:32 +0700

    Update

--- src/main/java/org/intellij/sdk/toolWindow/MyToolWindowFactory.java
+++ src/main/java/org/intellij/sdk/toolWindow/MyToolWindowFactory.java
@@ -5,9 +5,18 @@ package org.intellij.sdk.toolWindow;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.table.JBTable;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.util.Random;
 
 public class MyToolWindowFactory implements ToolWindowFactory {
 
@@ -18,10 +27,47 @@ public class MyToolWindowFactory implements ToolWindowFactory {
    * @param toolWindow current tool window
    */
   public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-    MyToolWindow myToolWindow = new MyToolWindow(toolWindow);
+
+    String data[][]={ {"1","2","3"},
+          {"1","2","3"},
+          {"1","2","3"}};
+    String columnNames[]={"Col1","Col2","Col3"};
+
+    JBTable table = new JBTable();
+
+    table.setDefaultRenderer(String.class, new BoardTableCellRenderer());
+    table.setVisible(true);
+    table.setBounds(30,40,200,300);
+
+    DefaultTableModel tableModel = new DefaultTableModel();
+    for(String columnName : columnNames){
+      tableModel.addColumn(columnName);
+    }
+    tableModel.addRow(new Object[]{"1", "2", "3"});
+    tableModel.addRow(new Object[]{"1", "2", "3"});
+
+    table.setModel(tableModel);
+
+    table.getColumn("Col2").setCellRenderer(new BoardTableCellRenderer());
+    table.setVisible(true);
+    table.setBounds(30,40,200,300);
+
     ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-    Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
+    Content content = contentFactory.createContent(table, "Table Name", false);
     toolWindow.getContentManager().addContent(content);
   }
 
+    public class BoardTableCellRenderer extends DefaultTableCellRenderer {
+
+      private static final long serialVersionUID = 1L;
+
+      public Component getTableCellRendererComponent(JBTable table, Object value, boolean isSelected, boolean hasFocus,int row,int col) {
+
+        Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+        Color randomColor = new Color(100, 200, 200);
+        c.setBackground(randomColor);
+
+        return c;
+      }
+  }
 }
